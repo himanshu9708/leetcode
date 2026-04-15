6e0d9a9b0c17907f9d8193256334f2964f00f5cf
himanshu9708 <153941524+himanshu9708@users.noreply.github.com>
2026-04-15 09:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (55.78%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxways(int i, String s,int n,int []dp){
+        if(i == n) return 1;
+        if(s.charAt(i) == '0') return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int result = maxways(i+1,s,n,dp);
+            if(i+1 < n){
+                if(s.charAt(i) == '1' ||( s.charAt(i) == '2' && s.charAt(i+1) <= '6' )){
+                    result =  result + maxways(i+2,s,n,dp);
+                }
+            }
+            return  dp[i] = result;
+        }
+    public int numDecodings(String s) {
+        int n = s.length();
+        int []dp = new int[n];
+        Arrays.fill(dp,-1);
+        return maxways(0,s,n,dp);
+    
+    }
+}
